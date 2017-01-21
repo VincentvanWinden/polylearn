a6115fa0a3fa7fa148476be55a79dfbde9fa2a7d
vincent.van.winden@gmail.com <kopers_hugodegroot@live.nl>
2017-01-21 07:25:53 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+var tmpl = document.querySelector('#tmpl');
+console.log(tmpl);
+tmpl.heading = 'Hello HomeOfRecords visitors!';
+tmpl.page = 'snooze';
+
+document.addEventListener('WebComponentsReady', function() {
+	var navicon = document.getElementById('navicon');
+	var drawerpanel = document.getElementById('drawerpanel');
+	navicon.addEventListener('click', function() {
+		console.log('yeah');
+		drawerpanel.togglePanel();
+	});
+});
